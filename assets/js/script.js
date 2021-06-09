5c417cc292bb6a4200e07ed26f3c66a5a0d42d77
Fiona-T <fitracey@gmail.com>
2021-06-09 16:38:03 +0000

    feat: add inital functionality for startGame() set category and show/hide els

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,19 +24,36 @@ window.onclick = function(event) {
 
 //when DOM has loaded, listen for button click on Adjective/Verb buttons to start game
 document.addEventListener("DOMContentLoaded", function () {
-    // variable holding the elements with class name type - these are the category buttons in word area
+    // variable holding html collection of elements with class name type - these are the category buttons in word area
     let types = document.getElementsByClassName("type");
-    // iterate and listen for click, on click check the innerHTML, to determine word type to get, if not verb or adjective, exit
+    // iterate and listen for click, on click check the innerHTML, to determine word type
     for (let type of types) {
         type.addEventListener('click', function () {
             let wordType = this.innerHTML;
-            if (wordType === "verb") {
-                console.log('get a verb'); //replace with function to get verb and start game
-            } else if (wordType === "adjective") {
-                console.log('get an adjective'); //replace with function to get adjective and start game
-            } else {
-                return;
-            }
+            startGame(wordType);
+            //add hidden class to the div holding these two buttons (verb and adjective)
+            this.parentElement.classList.add("hidden");
+            this.parentElement.previousElementSibling.classList.add("hidden");
         })
     }
 })
+
+/**
+ * start game - get word by type, show blank spaces for word, show hint button, hide wordtype buttons and intro text.
+ * @param {*verb or adjective} wordType 
+ */
+function startGame(wordType) {
+    if (wordType === "verb") { //change to ternary conditional?
+        console.log('get a verb'); //replace with function to get random verb to guess
+    } else if (wordType === 'adjective') {
+        console.log('get an adjective'); //replace with function to get random adj to guess
+    } else {
+        return; //if wordType is not either of these, exit
+    }
+    //show the div with text and hint button, show category
+    document.getElementById("word-area-in-play").classList.remove("hidden");
+    document.getElementById("category").innerText = `${wordType}`;
+    //show the words boxes - equal to length of the chosen word
+    //put focus on first keyboard item for convenience for keyboard users
+    document.getElementsByClassName("key")[0].focus();
+}
