714f2831d5c21550d07874dc860dc96834e08385
Fiona-T <fitracey@gmail.com>
2021-06-09 15:36:00 +0000

    feat: add inital word type selection in script.js and update index.html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,21 @@ window.onclick = function(event) {
     }
 }
 
+//when DOM has loaded, listen for button click on Adjective/Verb buttons to start game
+document.addEventListener("DOMContentLoaded", function () {
+    // variable holding the elements with class name type - these are the category buttons in word area
+    let types = document.getElementsByClassName("type");
+    // iterate and listen for click, on click check the innerHTML, to determine word type to get, if not verb or adjective, exit
+    for (let type of types) {
+        type.addEventListener('click', function () {
+            let wordType = this.innerHTML;
+            if (wordType === "verb") {
+                console.log('get a verb'); //replace with function to get verb and start game
+            } else if (wordType === "adjective") {
+                console.log('get an adjective'); //replace with function to get adjective and start game
+            } else {
+                return;
+            }
+        })
+    }
+})
