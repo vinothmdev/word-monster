1bde529cc4ce64cafbd06c024f70b9f27c94ebb8
Fiona-T <fitracey@gmail.com>
2021-07-06 15:03:27 +0000

    refactor: move const words outside of getWord function, words.js

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,13 +1,9 @@
 /* this file is used by index.html to get the random word by category when the category is chosen 
 from the initialiseCategories function in script.js and the getWord function is called.
-There is only one function in this file and it also contains all the words for the game.
+There is only one function in this file and all the words for the game are held in the const below.
+const words is outside of the getWord function so that it is not initialised each time the function is called
 Split out from script.js for readability */
 
-/**
- * Get random word by category from array of Word Objects(word, type, hint, meaning) then run startGame
- * @param {string} wordCategory Verb or Adjective
- */
-function getWord(wordCategory) {
 //array of word objects, each one has word, type, hint and meaning
 const words = [{
     word: "eat",
@@ -210,8 +206,14 @@ const words = [{
     hint: "the third colour of a rainbow",
     meaning: "of a colour like that of a lemon or gold or the sun"
 }];
-// filter the words array into a new array, by the chosen category
-let wordsByType = words.filter(word => word.type === wordCategory);
+
+/**
+ * Get random word by category from array of Word Objects(word, type, hint, meaning) then run startGame
+ * @param {string} wordCategory Verb or Adjective
+ */
+function getWord(wordCategory) {
+    // filter the words array into a new array, by the chosen category
+    let wordsByType = words.filter(word => word.type === wordCategory);
     //get a random word object from the wordsByType array, pass this to startGame function 
     let wordObject = wordsByType[Math.floor(Math.random() * wordsByType.length)];
     startGame(wordCategory, wordObject);
